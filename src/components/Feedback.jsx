2e8bb32a350f2d152941079f26ea71d62080d0d9
TheingiMamoru <theingi.nyunt.win@gmail.com>
2023-03-01 10:55:19 +0630

    responsive

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -23,7 +23,7 @@ const Feedback = () => {
     }
     console.log(toggle);
   return (
-    <div className={`${toggle && `animate__animated animate__fadeOutDown` } flex flex-col gap-3 w-[400px] h-fit rounded-[30px] shadow-xl p-[35px] bg-dark-blue`} >
+    <div className={`${toggle && `animate__animated animate__fadeOutDown` } flex flex-col gap-3 w-[400px] h-fit rounded-[30px] shadow-xl p-[35px] bg-dark-blue mx-5`} >
         <div className=' flex w-[45px] h-[45px] bg-medium-gray rounded-full items-center justify-center mb-5'>
             <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614"/></svg>
         </div>
